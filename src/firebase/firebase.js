afbfa505d9c095121d266ba0edb8b28977a98dac
oksanaliudukhovska <aldz84@ukr.net>
2025-03-02 13:40:25 +0100

    add  .env.local

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {getAuth, GoogleAuthProvider, FacebookAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCoZJvxEoTj3jmBHEv7dD4twM1SSVweZwA",
-    authDomain: "blossom-breeze.firebaseapp.com",
-    projectId: "blossom-breeze",
-    storageBucket: "blossom-breeze.firebasestorage.app",
-    messagingSenderId: "1051114246444",
-    appId: "1:1051114246444:web:9fd98f88923e0d3e045c89",
-    measurementId: "G-N4SY1MCB9Z"
-  };
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const facebookProvider = new FacebookAuthProvider();
- export default db;
+export default db;
